847473aff432f962fcb7deedf9d6d60bc17d0d16
KK-code-om <kkupse@inbox.lv>
2026-04-25 20:27:12 +0300

    V6.8: hard clean CSV import handlers

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "kk-calorie-app-v6-7";
+const CACHE_NAME = "kk-calorie-app-v6-8";
 
 const FILES_TO_CACHE = [
   "./",
-  "./index.html?v=6.7",
-  "./style.css?v=6.7",
-  "./app.js?v=6.7",
-  "./manifest.json?v=6.7",
+  "./index.html?v=6.8",
+  "./style.css?v=6.8",
+  "./app.js?v=6.8",
+  "./manifest.json?v=6.8",
   "./icon-192.png",
   "./icon-512.png"
 ];
